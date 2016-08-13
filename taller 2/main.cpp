9791fa9b2475e2228c3e2ac1661eccf0ce24bcba
Leidy Quintero <lyquintero94@gmail.com>
2016-08-13 13:45:56 -0500

    taller 2 operaciones matematicas

---
+++ taller 2/main.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    cout << "el resultado de -4*7+2*3/4-5 es " << -4*7+2*3/4-5 <<endl;
+
+    cout << "el resultado de 5*(3+4) es " << 5*(3+4) <<endl;
+
+    cout << "el resultado de 5*2+3*3 es " << 5*2+3*3 <<endl;
+
+    cout << "el resultado de (4+3)/(3+6/3) es " << (4+3)/(3+6/3) <<endl;
+
+    cout << " el resultado de 5/3*(3+5) es " << 5/3*(3+5) << endl;
+
+    // nos permite la elaboracion de  una operacion aritmetica
+
+return 0;
+}
